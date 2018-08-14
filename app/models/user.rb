f15261a29322467ef265ed4d60c6c5017c8906e9
Admin <da8y_01@yahoo.com>
2018-08-14 02:31:22 -0500

    customize devise user model, setup forms, create home page, seed db

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,15 @@ class User
   field :email,              type: String, default: ""
   field :encrypted_password, type: String, default: ""
 
+  field :name
+  validates_presence_of :name
+  validates_uniqueness_of :name, :email, :case_sensitive => false
+  # attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  # attr_accessor :name, :email, :password, :password_confirmation, :remember_me
+
+  field :password_confirmation
+  validates_presence_of :password_confirmation
+
   ## Recoverable
   field :reset_password_token,   type: String
   field :reset_password_sent_at, type: Time
@@ -33,4 +42,16 @@ class User
   # field :failed_attempts, type: Integer, default: 0 # Only if lock strategy is :failed_attempts
   # field :unlock_token,    type: String # Only if unlock strategy is :email or :both
   # field :locked_at,       type: Time
+
+  # private
+  # def user_params
+    # params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    # params.require(:user).permit(:email, :password, :password_confirmation, :name, owner_attributes: [:name])
+  #   params.require(:user).permit!
+  # end
+  # def sign_up_params
+    # params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    # params.require(:user).permit(:email, :password, :password_confirmation, :name, owner_attributes: [:name])
+    # params.require(:user).permit!
+  # end
 end
